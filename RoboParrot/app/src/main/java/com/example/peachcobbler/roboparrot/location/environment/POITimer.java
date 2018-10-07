6aa23212b9884438e651be967b87e4bc479f5622
seangovens <seangovens@gmail.com>
2018-10-07 15:04:22 -0600

    Interesting facts from Wikipedia.

---
+++ RoboParrot/app/src/main/java/com/example/peachcobbler/roboparrot/location/environment/POITimer.java
@@ -0,0 +1,31 @@
+package com.example.peachcobbler.roboparrot.location.environment;
+
+import android.os.Handler;
+import android.os.Message;
+
+import com.example.peachcobbler.roboparrot.location.ParrotLocationManager;
+
+public class POITimer extends Thread {
+    private long interval;
+    private long last;
+    private Handler bin;
+
+    public POITimer(long inter, Handler b) {
+        interval = inter; bin = b;
+        last = System.currentTimeMillis();
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            long curr = System.currentTimeMillis();
+            while (curr - last < interval) {
+                curr = System.currentTimeMillis();
+            }
+            Message msg = new Message();
+            msg.obj = ParrotLocationManager.current;
+            bin.sendMessage(msg);
+            last = System.currentTimeMillis();
+        }
+    }
+}
